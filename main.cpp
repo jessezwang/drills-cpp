887b066de4fbf25bd93f28cffa6bfb44b487c838
Zhe Wang <jesse.zhe.wang@gmail.com>
2012-10-21 12:16:10 -0400

    not ansi-style anymore.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 //============================================================================
-// Name        : drills-cpp.cpp
+// Name        : main.cpp
 // Author      : Zhe Wang
 // Version     :
 // Copyright   : GPL 3.0
-// Description : Hello World in C++, Ansi-style
+// Description : Hello World in C++
 //============================================================================
 
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 	cout << "!!!Hello!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
